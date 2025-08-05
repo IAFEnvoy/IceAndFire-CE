1cb8ffd6379198b4fd0bd8f06fc3fd393a5b47e0
IAFEnvoy <tanksheng545@126.com>
2025-08-05 16:51:32 +0800

    fix #79

--- common/src/main/java/com/iafenvoy/iceandfire/item/tool/DragonSteelToolMaterial.java
+++ common/src/main/java/com/iafenvoy/iceandfire/item/tool/DragonSteelToolMaterial.java
@@ -1,11 +1,12 @@
 package com.iafenvoy.iceandfire.item.tool;
 
+import com.iafenvoy.iceandfire.config.IafCommonConfig;
 import com.iafenvoy.uranus.object.item.ToolMaterialUtil;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ToolMaterial;
 
 public class DragonSteelToolMaterial {
-    public static ToolMaterial createMaterialWithRepairItem(ItemConvertible ingredient, String name) {
-        return ToolMaterialUtil.of(4000, 4F, 10F, 21, 10, ingredient);
+    public static ToolMaterial createMaterialWithRepairItem(ItemConvertible ingredient) {
+        return ToolMaterialUtil.of(IafCommonConfig.INSTANCE.armors.dragonSteelBaseDurability.getValue(), 4F, 10F, 21, 10, ingredient);
     }
 }
