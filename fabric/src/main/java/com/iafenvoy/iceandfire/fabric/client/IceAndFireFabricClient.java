64ab1b27ba2379451405735eef224787ba55ae92
IAFEnvoy <tanksheng545@126.com>
2026-08-10 00:31:48 +0800

    backport resource pack

--- fabric/src/main/java/com/iafenvoy/iceandfire/fabric/client/IceAndFireFabricClient.java
+++ fabric/src/main/java/com/iafenvoy/iceandfire/fabric/client/IceAndFireFabricClient.java
@@ -1,12 +1,21 @@
 package com.iafenvoy.iceandfire.fabric.client;
 
+import com.iafenvoy.iceandfire.IceAndFire;
 import com.iafenvoy.iceandfire.IceAndFireClient;
+import dev.architectury.platform.Platform;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public final class IceAndFireFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         IceAndFireClient.init();
         IceAndFireClient.process();
+        if (!Platform.isDevelopmentEnvironment())
+            FabricLoader.getInstance().getModContainer(IceAndFire.MOD_ID).ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(Identifier.of(IceAndFire.MOD_ID, "iaf_legacy"), container, Text.translatable("resourcePack.iceandfire.legacy.name"), ResourcePackActivationType.NORMAL));
     }
 }
--- forge/src/main/java/com/iafenvoy/iceandfire/forge/IceAndFireForgeClient.java
+++ forge/src/main/java/com/iafenvoy/iceandfire/forge/IceAndFireForgeClient.java
@@ -1,16 +1,22 @@
 package com.iafenvoy.iceandfire.forge;
 
+import com.iafenvoy.iceandfire.IceAndFire;
 import com.iafenvoy.iceandfire.IceAndFireClient;
 import com.iafenvoy.iceandfire.config.IafClientConfig;
 import com.iafenvoy.iceandfire.config.IafCommonConfig;
 import com.iafenvoy.jupiter.render.screen.ConfigSelectScreen;
+import dev.architectury.platform.Platform;
+import net.minecraft.resource.*;
 import net.minecraft.text.Text;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.resource.PathPackResources;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class IceAndFireForgeClient {
@@ -18,5 +24,19 @@ public class IceAndFireForgeClient {
     public static void init(FMLClientSetupEvent event) {
         event.enqueueWork(IceAndFireClient::process);
         ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> ConfigSelectScreen.builder(Text.translatable("config.iceandfire.title"), parent).server(IafCommonConfig.INSTANCE).client(IafClientConfig.INSTANCE).build()));
+    }
+
+    @SubscribeEvent
+    public static void onAddPackFinders(AddPackFindersEvent event) {
+        if (!Platform.isDevelopmentEnvironment() && event.getPackType() == ResourceType.CLIENT_RESOURCES)
+            event.addRepositorySource(consumer -> consumer.accept(ResourcePackProfile.create(
+                    "resourcepacks/iaf_legacy",
+                    Text.translatable("resourcePack.iceandfire.legacy.name"),
+                    false,
+                    name -> new PathPackResources(IceAndFire.MOD_ID, true, ModList.get().getModFileById(IceAndFire.MOD_ID).getFile().findResource(name)),
+                    ResourceType.CLIENT_RESOURCES,
+                    ResourcePackProfile.InsertionPosition.TOP,
+                    ResourcePackSource.BUILTIN
+            )));
     }
 }
