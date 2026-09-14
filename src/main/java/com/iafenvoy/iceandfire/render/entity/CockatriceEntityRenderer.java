417cabaec12e5bfb77894d5eea18f4273a10e39d
IAFEnvoy <tanksheng545@126.com>
2026-09-14 14:00:49 +0800

    minor fixes

--- src/main/java/com/iafenvoy/iceandfire/render/entity/CockatriceEntityRenderer.java
+++ src/main/java/com/iafenvoy/iceandfire/render/entity/CockatriceEntityRenderer.java
@@ -77,14 +77,6 @@ public class CockatriceEntityRenderer extends EntityRenderer<CockatriceEntity, L
     public void extractRenderState(CockatriceEntity entity, LegacyEntityRenderState<CockatriceEntity> state, float partialTicks) {
         super.extractRenderState(entity, state, partialTicks);
         state.entity = entity;
-        state.hasRedOverlay = entity.hurtTime > 0 || entity.deathTime > 0;
-        if (entity.isAlive()) {
-            state.walkAnimationPos = entity.walkAnimation.position(partialTicks);
-            state.walkAnimationSpeed = entity.walkAnimation.speed(partialTicks);
-        } else {
-            state.walkAnimationPos = 0.0F;
-            state.walkAnimationSpeed = 0.0F;
-        }
     }
 
     @Override
@@ -93,16 +85,15 @@ public class CockatriceEntityRenderer extends EntityRenderer<CockatriceEntity, L
         AdvancedEntityModel<CockatriceEntity> model = entity.isBaby() ? BABY_MODEL : ADULT_MODEL;
         poseStack.pushPose();
         // Match LivingEntityRenderer's model origin before handing rendering to the legacy model.
-        float bodyRot = Mth.rotLerp(state.partialTick, entity.yBodyRotO, entity.yBodyRot);
-        poseStack.mulPose(Axis.YP.rotationDegrees(180.0F - bodyRot));
+        poseStack.mulPose(Axis.YP.rotationDegrees(180.0F - state.bodyRot));
         poseStack.scale(-1.0F, -1.0F, 1.0F);
         this.scale(entity, poseStack);
         poseStack.translate(0.0F, -1.501F, 0.0F);
-        model.setupAnim(entity, state.walkAnimationPos, state.walkAnimationSpeed, state.ageInTicks, 0.0F, 0.0F);
+        model.setupAnim(entity, state.walkAnimationPos, state.walkAnimationSpeed, state.ageInTicks, state.yRot, state.xRot);
         collector.submitCustomGeometry(poseStack, RenderTypes.entityCutout(this.getTextureLocation(entity)), (pose, buffer) -> {
             PoseStack modelStack = new PoseStack();
             modelStack.last().set(pose);
-            model.renderToBuffer(modelStack, buffer, state.lightCoords, OverlayTexture.pack(OverlayTexture.u(0.0F), OverlayTexture.v(state.hasRedOverlay)), state.outlineColor == 0 ? -1 : state.outlineColor);
+            model.renderToBuffer(modelStack, buffer, state.lightCoords, OverlayTexture.pack(0, state.hasRedOverlay), state.outlineColor == 0 ? -1 : state.outlineColor);
         });
         poseStack.popPose();
         super.submit(state, poseStack, collector, camera);
